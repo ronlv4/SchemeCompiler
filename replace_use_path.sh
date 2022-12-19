ab6392be935d0fc3a99eef52085ce8b00d44fdf2
Ron Levi <t-ronlevi@microsoft.com>
2022-12-19 17:08:26 +0200

    replace path script

---
+++ replace_use_path.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ron_path="/home/ronlv4/repos/compilation_assignment"
+or_path="/home/orzad/repos/compilation_assignment"
+
+if [[ $1 == "ron" ]]; then
+  current_path=$or_path
+  path=$ron_path
+else
+  current_path=$ron_path
+  path=$or_path
+fi
+
+for file in $(find ${path} -name "*.c" -o -name "*.h")
+do
+  sed -i "s|${current_path}|${path}|g" $file
+done
