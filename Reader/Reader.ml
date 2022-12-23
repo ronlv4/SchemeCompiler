53cc2b800c3e8166be1e47dc8e1ca882ed494805
Ron Levi <t-ronlevi@microsoft.com>
2022-12-23 23:33:26 +0200

    fix symbol v4

--- Reader/Reader.ml
+++ Reader/Reader.ml
@@ -273,7 +273,6 @@ module Reader : READER = struct
     let nt2 = one_of ".+@-" in
     let char_digit = range '0' '9' in
     let letter = range_ci 'a' 'z' in
-    let letter = pack letter Char.lowercase_ascii in
     let initial = disj letter nt1 in
     let subsequent = disj initial (disj char_digit nt2) in
     let nt1 = caten initial (star subsequent) in
