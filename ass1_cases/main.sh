fb740f15515bdaac5c5f87e1f377359f441487ab
Ron Levi <t-ronlevi@microsoft.com>
2022-12-28 23:00:14 +0200

    main script for testing

---
+++ ass1_cases/main.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+dir_student="in_to_student"
+dir_scheme="in_to_scheme"
+
+all_passed=true
+for file_name in $(ls ${dir_student})
+do
+	student_string=$(cat ${dir_student}/${file_name})
+	scheme_string=$(cat ${dir_scheme}/${file_name}) 
+	if [ "${scheme_string}" != "${student_string}" ]; then
+		echo "Failed test"
+		echo "scheme string: ${scheme_string}"
+		echo -e "student string: ${student_string}\n\n"
+		all_passed=false
+	fi
+done
+
+if [ ${all_passed} = true ]; then
+	echo "all tests passed successfuly"
+fi
+
