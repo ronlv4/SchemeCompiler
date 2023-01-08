75dac9e9d1177a69d02c489de582c3df688f7fc5
Ron Levi <t-ronlevi@microsoft.com>
2023-01-08 23:08:08 +0200

    add ass2 official tests-fix

--- ass2_cases/tester.ml
+++ ass2_cases/tester.ml
@@ -5,7 +5,7 @@ let rec run_test = function
   | [] -> "finished testing successfully"
   | (code, sexpr, expr, expr'):: tl ->
     let result_expr = Tag_Parser.tag_parse sexpr in
-    if (result_expr = expr) then run_test tl else (Printf.printf "failed on test with %s" code)
+    if (result_expr = expr) then run_test tl else (Printf.sprintf "failed on test with %s" code)
 
 
 run_test [("a", ScmSymbol "a", ScmVarGet (Var "a"), ScmVarGet' (Var' ("a", Free)))]
